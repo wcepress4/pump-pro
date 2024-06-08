6261e208c251376ebd403087b87341f07afb39bf
wcepress4 <wcepress@yahoo.com>
2024-06-08 06:14:16 -0400

    initial commit with List Users table

---
+++ src/components/ListUserComponent.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+const ListUserComponent = () => {
+
+    const [users, setUsers] = useState([])
+
+    return (
+        <div className = "container">
+            <h2 className='text-center'>List Users</h2>
+            <table className='table table-bordered table-striped'>
+                <thead>
+                    <th> User Id </th>
+
+                    <th> User First Name </th>
+                    <th> User Last Name </th>
+                    <th> User Email Id </th>
+                </thead>
+                <tbody>
+                    {
+                        users.map(
+                            user =>
+                            <tr key = {user.id}>
+                                <td> {user.id} </td>
+                                <td> {user.firstName} </td>
+                                <td> {user.lastName} </td>
+                                <td> {user.emailId} </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>            
+        </div>
+    )
+}
+
+export default ListUserComponent
