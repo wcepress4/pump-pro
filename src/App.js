bc241ea81b914c3d213dfdca72db6f9d24e8eb5a
wcepress4 <wcepress@yahoo.com>
2024-06-09 18:11:05 -0400

    created basic header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 import ListUserComponent from './components/ListUserComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <ListUserComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className = "navbar navabr-expand-md navbar-dark bg-dark">
+                <div>
+                    <a href="https://wcepress.me" className='navbar-brand'>
+                        User Management Application
+                    </a>
+                </div>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
